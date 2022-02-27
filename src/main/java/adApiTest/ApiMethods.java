136caaba32628c5da4d441f3b6c8aefa73c7e812
ankitzone <ankittyagizone@gmail.com>
2022-02-27 18:55:08 +0100

    initial commit

---
+++ src/main/java/adApiTest/ApiMethods.java
@@ -0,0 +1,69 @@
+package adApiTest;
+
+import org.json.simple.JSONObject;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class ApiMethods {
+	String baseURL = "https://admin-advertisement.herokuapp.com/api";
+
+	public Response postrequest(String name, String street, int room, double price, boolean status) {
+		RestAssured.baseURI = baseURL;
+
+		RequestSpecification request = RestAssured.given();
+
+		JSONObject requestParams = new JSONObject();
+		requestParams.put("name", name);
+		requestParams.put("street", street);
+		requestParams.put("room", room);
+		requestParams.put("price", price);
+		requestParams.put("status", status);
+
+		request.header("Content-Type", "application/json");
+		request.body(requestParams.toJSONString());
+
+		Response response = request.post("/advertisements");
+
+		return response;
+
+	}
+
+	public Response updateAdByName(String id, String name) {
+
+		RestAssured.baseURI = baseURL;
+
+		RequestSpecification request = RestAssured.given();
+
+		JSONObject updateRequestParams = new JSONObject();
+		updateRequestParams.put("name", name);
+
+		request.header("Content-Type", "application/json");
+		request.body(updateRequestParams.toJSONString());
+
+		Response updatedResponse = request.put("/advertisements/" + id);
+
+		return updatedResponse;
+
+	}
+	
+	
+	public Response updateAdByStreet(String id, String Street) {
+
+		RestAssured.baseURI = baseURL;
+
+		RequestSpecification request = RestAssured.given();
+
+		JSONObject updateRequestParams = new JSONObject();
+		updateRequestParams.put("street", Street);
+
+		request.header("Content-Type", "application/json");
+		request.body(updateRequestParams.toJSONString());
+
+		Response updatedResponse = request.put("/advertisements/" + id);
+
+		return updatedResponse;
+
+	}
+
+}
---
+++ src/main/java/adApiTest/TestCases.java
@@ -0,0 +1,74 @@
+package adApiTest;
+
+import org.junit.Assert;
+import org.testng.annotations.Test;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
+public class TestCases {
+
+	@Test(description = "Create new advertisement with valid data")
+	public void CreateNewAd() {
+
+		ApiMethods apiObj = new ApiMethods();
+
+		Response response = apiObj.postrequest("TestName1", "Raddison Street", 25, 1000, true);
+
+		Assert.assertTrue("failed request", response.getStatusCode() == 200);
+		String body = response.getBody().asString();
+		JsonPath jsonPath = new JsonPath(body);
+		String adId = jsonPath.getString("_id");
+
+		System.out.println("This is response  :" + adId);
+
+	}
+
+	@Test(description = "Edit any existing advertisement by name")
+	public void updateAdByName() {
+		ApiMethods apiObj = new ApiMethods();
+
+		Response response = apiObj.postrequest("TestNameUpdate1", "Raddison Street", 25, 1000, true);
+
+		Assert.assertTrue("failed request", response.getStatusCode() == 200);
+
+		String body = response.getBody().asString();
+		JsonPath jsonPath = new JsonPath(body);
+		String adId = jsonPath.getString("_id");
+
+		System.out.println("This is unique id  :" + adId);
+
+		Response updatedResponse = apiObj.updateAdByName(adId, "TestNameUpdateFinally");
+
+		Assert.assertTrue("failed updated request", updatedResponse.getStatusCode() == 200);
+
+		String updatedResponseBody = updatedResponse.getBody().asString();
+
+		System.out.println("This is update response  :" + updatedResponseBody);
+
+	}
+
+	@Test(description = "Edit existing advertisement by street")
+	public void updateAdByStreet() {
+		ApiMethods apiObj = new ApiMethods();
+
+		Response response = apiObj.postrequest("TestStreetUpdate1", "Taj Street", 25, 1000, true);
+
+		Assert.assertTrue("failed request", response.getStatusCode() == 200);
+
+		String body = response.getBody().asString();
+		JsonPath jsonPath = new JsonPath(body);
+		String adId = jsonPath.getString("_id");
+
+		System.out.println("This is unique id  :" + adId);
+
+		Response updatedResponse = apiObj.updateAdByStreet(adId, "Hyatt Street");
+
+		Assert.assertTrue("failed updated request", updatedResponse.getStatusCode() == 200);
+
+		String updatedResponseBody = updatedResponse.getBody().asString();
+
+		System.out.println("This is update by street  :" + updatedResponseBody);
+
+	}
+
+}
